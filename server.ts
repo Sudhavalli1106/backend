63acb496baf6dc9a8d6f2f4fb40b56dac7317729
Sudhavalli1106 <sudhavalli.parthasarathy@matrimony.com>
2025-04-01 12:55:56 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
-console.log("learning on collaboration")
+console.log("collaboration with fullstack")
 
 console.log("adding new code")
-<<<<<<< HEAD
+
 console.log("3rd line added")
-=======
->>>>>>> 7b35bbded2328294c831d007e18e1a94b1b45070
+console.log("4th line added")
